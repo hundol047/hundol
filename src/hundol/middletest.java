4080406ebc9bf986943c569bce1629595c1cfb17
hundol047 <hundol047@gmail.com>
2024-10-23 14:16:16 +0900

    안녕하세요

--- src/hundol/middletest.java
+++ src/hundol/middletest.java
@@ -1,6 +1,24 @@
 package hundol;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
 
-public class middletest extends JFrame {
+public class middletest extends JFrame implements ActionListener, KeyListener {
+    private JTextField dispaly;
+    private JTextField operator;
+    private double firstNumber;
+
+    @Override
+    public void setTitle(String title) {
+        super.setTitle("계산기");
+    }
+
+    @Override
+    public void setSize(int width, int height) {
+        super.setSize(300, 400);
+    }
 }
+
+
