2dcc4ad642d4307ada1c3c8035a191dba1c46666
hundol047 <hundol047@gmail.com>
2024-10-23 15:09:20 +0900

    안녕하세요

--- src/hundol/middletest.java
+++ src/hundol/middletest.java
@@ -10,7 +10,7 @@ public class middletest extends JFrame implements ActionListener, KeyListener {
     private JTextField operator;
     private double firstNumber;
 
-    @Override
+    @Override //프레임 만들기
     public void setTitle(String title) {
         super.setTitle("계산기");
     }
@@ -19,6 +19,28 @@ public class middletest extends JFrame implements ActionListener, KeyListener {
     public void setSize(int width, int height) {
         super.setSize(300, 400);
     }
+
+    @Override
+    public void setDefaultCloseOperation(int operation) {
+        super.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    @Override
+    public void setLayout(LayoutManager manager) {
+        super.setLayout(new BorderLayout());
+    }
+
+    display = new JTextField();
+    display.setEditable(false);
+    display.setHorizontalAlignment(SwingConstants.RIGHT);
+    display.setBackground(Color.WHITE);
+    display.setPreferredSize(new Dimension(300, 65));
+    add(display, BorderLayout.NORTH);
+
+    public static void main(String[] args) {
+        middletest f = new middletest();
+    }
+
 }
 
 
